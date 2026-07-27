c0df09c545caec35284e910c881a6860f86a5bad
jackkerk2021 <jackkerk2021@gmail.com>
2026-07-27 13:48:38 +0800

    Update world_state.py

--- src/state/world_state.py
+++ src/state/world_state.py
@@ -2,10 +2,7 @@ class WorldState:
 
     def __init__(self):
         self.characters = {}
-        self.objects = {"Flashlight": {
-
-        "owner": "Jack"
-}
+        self.objects = {}
         self.locations = {}
 
 feat(state): add WorldState
