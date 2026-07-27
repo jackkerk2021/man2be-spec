352af7520b64a8e51d57e0d8f71a1087063c780b
jackkerk2021 <jackkerk2021@gmail.com>
2026-07-27 13:25:27 +0800

    Update runtime.py

--- src/runtime/runtime.py
+++ src/runtime/runtime.py
@@ -1,1 +1,15 @@
-hi
+state = WorldState()
+
+engine = StateEngine()
+
+event = ObjectEvent(
+
+    actor="Jack",
+
+    action="PickUp",
+
+    target="Flashlight"
+
+)
+
+engine.apply(state, event)
