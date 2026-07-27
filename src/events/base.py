82d46ab6ed7421c58b9503e9ec802bb18f65c373
jackkerk2021 <jackkerk2021@gmail.com>
2026-07-27 13:47:29 +0800

    Update base.py

--- src/events/base.py
+++ src/events/base.py
@@ -4,9 +4,7 @@ from uuid import UUID, uuid4
 
 @dataclass(frozen=True)
 class Event:
-
     id: UUID = field(default_factory=uuid4)
-
     type: str = ""
 
 feat(events): add immutable Event base class
