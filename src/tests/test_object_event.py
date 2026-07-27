ac02326e506162c6606d4fb83d0387d538553ef7
jackkerk2021 <jackkerk2021@gmail.com>
2026-07-27 13:23:06 +0800

    Update test_object_event.py

--- src/tests/test_object_event.py
+++ src/tests/test_object_event.py
@@ -1,5 +1,32 @@
 def test_pickup_flashlight():
 
-    ...
+  from events.object import ObjectEvent
+from state.world_state import WorldState
+from state.state_engine import StateEngine
+
+
+def test_pickup_flashlight():
+
+    state = WorldState()
+
+    engine = StateEngine()
+
+    event = ObjectEvent(
+
+        id=None,
+
+        type="ObjectEvent",
+
+        actor="Jack",
+
+        action="PickUp",
+
+        target="Flashlight"
+
+    )
+
+    engine.apply(state, event)
+
+    assert state.objects["Flashlight"]["owner"] == "Jack"
 
     assert ...
